a338626ae24b867efa9a77575925440163d390a3
Albert Safin <albertsafinf@gmail.com>
2024-02-13 21:32:00 +0500

    New app small_prod_card and its model Product

--- kamalsite/kamalsite/settings.py
+++ kamalsite/kamalsite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'small_prod_card.apps.SmallProdCardConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- kamalsite/small_prod_card/models.py
+++ kamalsite/small_prod_card/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.TextField()
+    likes = models.IntegerField("times liked", default=0)
+    additions = models.IntegerField("additions to cart", default=0)
+
+    def __str__(self):
+        return (f"{self.name}("
+                f"Likes={self.likes}, Additions={self.additions})")
