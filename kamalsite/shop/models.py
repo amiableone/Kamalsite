c38fa6897edc1a531cc4d03d87d9f12c93024864
Albert Safin <albertsafinf@gmail.com>
2024-02-16 13:52:39 +0500

    Added purchases counter to Product model

--- kamalsite/shop/models.py
+++ kamalsite/shop/models.py
@@ -5,7 +5,7 @@ class Product(models.Model):
     description = models.TextField()
     likes = models.IntegerField("times liked", default=0)
     additions = models.IntegerField("additions to cart", default=0)
+    purchases = models.IntegerField("times purchased", default=0)
 
     def __str__(self):
-        return (f"{self.name}("
-                f"Likes={self.likes}, Additions={self.additions})")
+        return (f"{self.name.title()}(Likes={self.likes})")
