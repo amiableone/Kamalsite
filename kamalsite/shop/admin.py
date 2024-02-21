c191a16efc3dcfcd7acc9974cf592d2c661229ba
Albert Safin <albertsafinf@gmail.com>
2024-02-21 22:40:07 +0500

    Custom shop app admin site

--- kamalsite/shop/admin.py
+++ kamalsite/shop/admin.py
@@ -2,4 +2,13 @@ from django.contrib import admin
 
 from .models import Product
 
-admin.site.register(Product)
+
+class MyAdminSite(admin.AdminSite):
+    site_title = "Power place"
+    site_header = "МЕСТО СИЛЫ"
+    index_title = "Админка Камалсайта"
+
+
+admin_site = MyAdminSite(name="kamaladmin")
+
+admin_site.register(Product)
