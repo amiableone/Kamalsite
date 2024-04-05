a039a8e7135752bd9770794b5482455c564ced1f
Albert Safin <albertsafinf@gmail.com>
2024-04-05 16:26:58 +0500

    Add PermissionsMixin into User class hierarchy.

--- kamalsite/myauth/models.py
+++ kamalsite/myauth/models.py
@@ -1,12 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, PermissionsMixin
 
-# TODO:
-#   To make site useful in tracking inventory and production
-#   status based on both retail and wholesale sales, it is
-#   likely that admins will have to create wholesale orders
-#   on behalf of some wholesale clients that will not bother
-#   themselves with these CAN'T REMEMBER THE TERM.
-
-class User(AbstractUser):
+class User(AbstractUser, PermissionsMixin):
     pass
