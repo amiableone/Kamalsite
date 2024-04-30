371fdf29ccdf679e64b7eec36025d45ba4e6e138
Albert Safin <albertsafinf@gmail.com>
2024-04-30 20:14:36 +0500

    Create ProductCardLikeView to handle likes from catalog page, Customize get() of CatalogView to couple like forms to corresponding products, Customize post() of CatalogView to handle likes, Customize get_context_data() of ProductDetailView, Add a urlpattern product-card-like for handling likes, Update catalog.html template to make likes work.

--- kamalsite/shop/urls.py
+++ kamalsite/shop/urls.py
@@ -1,12 +1,22 @@
 from django.urls import path
 from django.views.generic.base import RedirectView
 
-from .views import NoPageRedirectView, CatalogView, ProductDetailView
+from .views import (
+    NoPageRedirectView,
+    CatalogView,
+    ProductCardLikeView,
+    ProductDetailView,
+)
 
 app_name = "shop"
 urlpatterns = [
     # redirect shop to catalog with page=1:
     path("", NoPageRedirectView.as_view(), name="shop"),
     path("page<int:page>/", CatalogView.as_view(), name="catalog"),
+    path(
+        "like-<int:product_id>/",
+        CatalogView.as_view(),
+        name="product-card-like",
+    ),
     path("product<int:pk>/", ProductDetailView.as_view(), name="product-detail"),
 ]
--- kamalsite/shop/views.py
+++ kamalsite/shop/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.http import (
+    HttpResponse,
+    Http404,
+    HttpResponseForbidden,
+    HttpResponseRedirect,
+)
 from django.template import loader
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
+from django.views import View
 from django.views.generic import DetailView, ListView
 from django.views.generic.base import RedirectView
 
@@ -13,7 +19,6 @@ from .forms import (
 )
 from .models import (
     Product,
-    User,
     Like,
 )
 
@@ -35,15 +40,64 @@ class CatalogView(ListView):
     paginate_by = 4
     queryset = Product.objects.filter(in_production=True)
     template_name = "shop/catalog.html"
+    filter_settings = {}
+    sort_settings = {}
+
+    def get(self, request, *args, **kwargs):
+        like_form = LikeForm
+        like_forms = []
+        for product in self.queryset:
+            like_forms.append(
+                like_form(
+                    initial={"user": request.user.id, "product": product.id},
+                )
+            )
+        # Other things like addition_form will be put in self.product_cards later.
+        self.product_cards = zip(self.queryset, like_forms)
+        request.session["page"] = kwargs["page"]
+        return super().get(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        view = ProductCardLikeView.as_view()
+        return view(request, *args, **kwargs)
+
+    post.alters_data = True
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["filter_form"] = CatalogFilterForm
-        context["sort_form"] = CatalogSortForm
-        context["like_form"] = LikeForm
-        context["apply"] = _("Apply")
-        context["like"] = _("Like")
+        context["filter_form"] = CatalogFilterForm()
+        context["sort_form"] = CatalogSortForm()
+        context["apply_button"] = _("Apply")
+        context["like_button"] = _("Like")
         return context
+
+
+class ProductCardLikeView(View):
+    form_class = LikeForm
+    initial = {}
+    template_name = "shop/catalog.html"
+
+    def post(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return HttpResponseForbidden()
+        try:
+            like = Like.objects.get(
+                user=request.user,
+                product=kwargs["product_id"],
+            )
+        except Like.DoesNotExist:
+            product = get_object_or_404(Product, id=kwargs["product_id"])
+            like = Like(user=request.user, product=product)
+            like.save()
+        except KeyError:
+            return Http404("Product does not exist.")
+        form = self.form_class(request.POST, instance=like)
+        page = request.session.get("page", 1)
+        if form.is_valid():
+            form.save()
+        return HttpResponseRedirect(
+            reverse("shop:catalog", kwargs={"page": page})
+        )
 
 
 class ProductDetailView(DetailView):
@@ -52,3 +106,10 @@ class ProductDetailView(DetailView):
     """
     context_object_name = "product"
     queryset = Product.objects.filter(in_production=True)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["like"] = _("Like")
+        context["add_to_cart_button"] = _("Add to cart")
+        context["buy_now_button"] = _("Buy now")
+        return context
