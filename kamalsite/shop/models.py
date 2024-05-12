ed746f33257a2a6a16981fc78025a22fe84e10a1
Albert Safin <albertsafinf@gmail.com>
2024-05-12 14:23:17 +0500

    Fix get_sentinel_user returning a tuple instead of a User in shop.models.

--- kamalsite/shop/models.py
+++ kamalsite/shop/models.py
@@ -11,7 +11,7 @@ from django.db import models
 #       amounts.
 
 def get_sentinel_user():
-    return get_user_model().objects.get_or_create(username="deleted")
+    return get_user_model().objects.get_or_create(username="deleted")[0]
 
 
 class Product(models.Model):
